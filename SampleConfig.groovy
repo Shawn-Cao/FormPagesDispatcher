2446d457db5d760e3bf65be465a779e5b97afe18
Shawn-Cao <xiangke.cao@gmail.com>
2015-08-16 23:18:21 -0400

    Using string splice for configuarion

--- SampleConfig.groovy
+++ SampleConfig.groovy
@@ -6,6 +6,7 @@ forms {
 }
 
 workflows {
+        upgradeRegular = 'eligibility,dradeIn,shopping'
 	changeSim {
         entryPoint = deviceSubscriberPostpaid.gsp  //landing page  //1. analysis landing page or 2. give requirement to find best landing page?
 		toGather = newSimNumber
